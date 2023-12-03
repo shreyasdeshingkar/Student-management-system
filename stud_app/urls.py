270dcb90d2c576cf69aa4237d850c8ea8c18bf1a
shreyas <shreyasdeshingkar@gmail.com>
2023-12-03 16:30:33 +0530

    logout button and changes are done

---
+++ stud_app/urls.py
@@ -0,0 +1,18 @@
+from . import views
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('',views.home,name='homepage'),
+    path('contact/',views.contact,name='contact'),
+    path('about/',views.about,name='about'),
+    path('our_department/',views.our_department,name='our_department'),
+    path('year_log/',views.year_log,name='year_log'),
+    path('stud_mgt/',views.stud_mgt,name='stud_mgt'),
+
+    path('dashboard/',views.dashboard,name='dashboard')
+
+
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ stud_app/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'index.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def our_department(request):
+    return render(request, 'our_department.html')
+
+def dashboard(request):
+    return render(request, 'admin dashboard/dashboard.html')
+
+def year_log(request):
+     return render(request, 'admin dashboard/year_log.html')
+ 
+def stud_mgt(request):
+    return render(request, 'admin dashboard/stud_mgt.html')
